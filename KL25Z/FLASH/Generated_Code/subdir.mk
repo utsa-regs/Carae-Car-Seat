7c78c61230db05858fa5ec315b66a241a96b94a9
inaruslynx <inaruslynx@gmail.com>
2019-03-09 01:02:55 -0600

    Changed gitignore
    Turns out Github was ignoring some of the folders in the gitignore. Changed and now all files are uploaded

---
+++ KL25Z/FLASH/Generated_Code/subdir.mk
@@ -0,0 +1,354 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+-include ../makefile.local
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS_QUOTED += \
+"../Generated_Code/BitIoLdd1.c" \
+"../Generated_Code/BitIoLdd2.c" \
+"../Generated_Code/BitIoLdd3.c" \
+"../Generated_Code/BitIoLdd4.c" \
+"../Generated_Code/BitIoLdd5.c" \
+"../Generated_Code/CE.c" \
+"../Generated_Code/CSN.c" \
+"../Generated_Code/Cpu.c" \
+"../Generated_Code/ExtIntLdd1.c" \
+"../Generated_Code/IRQ.c" \
+"../Generated_Code/LEDB.c" \
+"../Generated_Code/LEDG.c" \
+"../Generated_Code/LEDR.c" \
+"../Generated_Code/LEDpin1.c" \
+"../Generated_Code/LEDpin2.c" \
+"../Generated_Code/LEDpin3.c" \
+"../Generated_Code/MCUC1.c" \
+"../Generated_Code/PE_LDD.c" \
+"../Generated_Code/SM1.c" \
+"../Generated_Code/SMasterLdd1.c" \
+"../Generated_Code/Vectors.c" \
+"../Generated_Code/WAIT1.c" \
+
+C_SRCS += \
+../Generated_Code/BitIoLdd1.c \
+../Generated_Code/BitIoLdd2.c \
+../Generated_Code/BitIoLdd3.c \
+../Generated_Code/BitIoLdd4.c \
+../Generated_Code/BitIoLdd5.c \
+../Generated_Code/CE.c \
+../Generated_Code/CSN.c \
+../Generated_Code/Cpu.c \
+../Generated_Code/ExtIntLdd1.c \
+../Generated_Code/IRQ.c \
+../Generated_Code/LEDB.c \
+../Generated_Code/LEDG.c \
+../Generated_Code/LEDR.c \
+../Generated_Code/LEDpin1.c \
+../Generated_Code/LEDpin2.c \
+../Generated_Code/LEDpin3.c \
+../Generated_Code/MCUC1.c \
+../Generated_Code/PE_LDD.c \
+../Generated_Code/SM1.c \
+../Generated_Code/SMasterLdd1.c \
+../Generated_Code/Vectors.c \
+../Generated_Code/WAIT1.c \
+
+OBJS += \
+./Generated_Code/BitIoLdd1.o \
+./Generated_Code/BitIoLdd2.o \
+./Generated_Code/BitIoLdd3.o \
+./Generated_Code/BitIoLdd4.o \
+./Generated_Code/BitIoLdd5.o \
+./Generated_Code/CE.o \
+./Generated_Code/CSN.o \
+./Generated_Code/Cpu.o \
+./Generated_Code/ExtIntLdd1.o \
+./Generated_Code/IRQ.o \
+./Generated_Code/LEDB.o \
+./Generated_Code/LEDG.o \
+./Generated_Code/LEDR.o \
+./Generated_Code/LEDpin1.o \
+./Generated_Code/LEDpin2.o \
+./Generated_Code/LEDpin3.o \
+./Generated_Code/MCUC1.o \
+./Generated_Code/PE_LDD.o \
+./Generated_Code/SM1.o \
+./Generated_Code/SMasterLdd1.o \
+./Generated_Code/Vectors.o \
+./Generated_Code/WAIT1.o \
+
+OBJS_QUOTED += \
+"./Generated_Code/BitIoLdd1.o" \
+"./Generated_Code/BitIoLdd2.o" \
+"./Generated_Code/BitIoLdd3.o" \
+"./Generated_Code/BitIoLdd4.o" \
+"./Generated_Code/BitIoLdd5.o" \
+"./Generated_Code/CE.o" \
+"./Generated_Code/CSN.o" \
+"./Generated_Code/Cpu.o" \
+"./Generated_Code/ExtIntLdd1.o" \
+"./Generated_Code/IRQ.o" \
+"./Generated_Code/LEDB.o" \
+"./Generated_Code/LEDG.o" \
+"./Generated_Code/LEDR.o" \
+"./Generated_Code/LEDpin1.o" \
+"./Generated_Code/LEDpin2.o" \
+"./Generated_Code/LEDpin3.o" \
+"./Generated_Code/MCUC1.o" \
+"./Generated_Code/PE_LDD.o" \
+"./Generated_Code/SM1.o" \
+"./Generated_Code/SMasterLdd1.o" \
+"./Generated_Code/Vectors.o" \
+"./Generated_Code/WAIT1.o" \
+
+C_DEPS += \
+./Generated_Code/BitIoLdd1.d \
+./Generated_Code/BitIoLdd2.d \
+./Generated_Code/BitIoLdd3.d \
+./Generated_Code/BitIoLdd4.d \
+./Generated_Code/BitIoLdd5.d \
+./Generated_Code/CE.d \
+./Generated_Code/CSN.d \
+./Generated_Code/Cpu.d \
+./Generated_Code/ExtIntLdd1.d \
+./Generated_Code/IRQ.d \
+./Generated_Code/LEDB.d \
+./Generated_Code/LEDG.d \
+./Generated_Code/LEDR.d \
+./Generated_Code/LEDpin1.d \
+./Generated_Code/LEDpin2.d \
+./Generated_Code/LEDpin3.d \
+./Generated_Code/MCUC1.d \
+./Generated_Code/PE_LDD.d \
+./Generated_Code/SM1.d \
+./Generated_Code/SMasterLdd1.d \
+./Generated_Code/Vectors.d \
+./Generated_Code/WAIT1.d \
+
+OBJS_OS_FORMAT += \
+./Generated_Code/BitIoLdd1.o \
+./Generated_Code/BitIoLdd2.o \
+./Generated_Code/BitIoLdd3.o \
+./Generated_Code/BitIoLdd4.o \
+./Generated_Code/BitIoLdd5.o \
+./Generated_Code/CE.o \
+./Generated_Code/CSN.o \
+./Generated_Code/Cpu.o \
+./Generated_Code/ExtIntLdd1.o \
+./Generated_Code/IRQ.o \
+./Generated_Code/LEDB.o \
+./Generated_Code/LEDG.o \
+./Generated_Code/LEDR.o \
+./Generated_Code/LEDpin1.o \
+./Generated_Code/LEDpin2.o \
+./Generated_Code/LEDpin3.o \
+./Generated_Code/MCUC1.o \
+./Generated_Code/PE_LDD.o \
+./Generated_Code/SM1.o \
+./Generated_Code/SMasterLdd1.o \
+./Generated_Code/Vectors.o \
+./Generated_Code/WAIT1.o \
+
+C_DEPS_QUOTED += \
+"./Generated_Code/BitIoLdd1.d" \
+"./Generated_Code/BitIoLdd2.d" \
+"./Generated_Code/BitIoLdd3.d" \
+"./Generated_Code/BitIoLdd4.d" \
+"./Generated_Code/BitIoLdd5.d" \
+"./Generated_Code/CE.d" \
+"./Generated_Code/CSN.d" \
+"./Generated_Code/Cpu.d" \
+"./Generated_Code/ExtIntLdd1.d" \
+"./Generated_Code/IRQ.d" \
+"./Generated_Code/LEDB.d" \
+"./Generated_Code/LEDG.d" \
+"./Generated_Code/LEDR.d" \
+"./Generated_Code/LEDpin1.d" \
+"./Generated_Code/LEDpin2.d" \
+"./Generated_Code/LEDpin3.d" \
+"./Generated_Code/MCUC1.d" \
+"./Generated_Code/PE_LDD.d" \
+"./Generated_Code/SM1.d" \
+"./Generated_Code/SMasterLdd1.d" \
+"./Generated_Code/Vectors.d" \
+"./Generated_Code/WAIT1.d" \
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Generated_Code/BitIoLdd1.o: ../Generated_Code/BitIoLdd1.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #8 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/BitIoLdd1.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/BitIoLdd1.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/BitIoLdd2.o: ../Generated_Code/BitIoLdd2.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #9 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/BitIoLdd2.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/BitIoLdd2.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/BitIoLdd3.o: ../Generated_Code/BitIoLdd3.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #10 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/BitIoLdd3.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/BitIoLdd3.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/BitIoLdd4.o: ../Generated_Code/BitIoLdd4.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #11 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/BitIoLdd4.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/BitIoLdd4.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/BitIoLdd5.o: ../Generated_Code/BitIoLdd5.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #12 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/BitIoLdd5.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/BitIoLdd5.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/CE.o: ../Generated_Code/CE.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #13 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/CE.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/CE.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/CSN.o: ../Generated_Code/CSN.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #14 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/CSN.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/CSN.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/Cpu.o: ../Generated_Code/Cpu.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #15 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/Cpu.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/Cpu.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/ExtIntLdd1.o: ../Generated_Code/ExtIntLdd1.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #16 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/ExtIntLdd1.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/ExtIntLdd1.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/IRQ.o: ../Generated_Code/IRQ.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #17 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/IRQ.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/IRQ.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/LEDB.o: ../Generated_Code/LEDB.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #18 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/LEDB.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/LEDB.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/LEDG.o: ../Generated_Code/LEDG.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #19 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/LEDG.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/LEDG.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/LEDR.o: ../Generated_Code/LEDR.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #20 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/LEDR.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/LEDR.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/LEDpin1.o: ../Generated_Code/LEDpin1.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #21 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/LEDpin1.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/LEDpin1.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/LEDpin2.o: ../Generated_Code/LEDpin2.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #22 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/LEDpin2.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/LEDpin2.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/LEDpin3.o: ../Generated_Code/LEDpin3.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #23 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/LEDpin3.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/LEDpin3.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/MCUC1.o: ../Generated_Code/MCUC1.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #24 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/MCUC1.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/MCUC1.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/PE_LDD.o: ../Generated_Code/PE_LDD.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #25 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/PE_LDD.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/PE_LDD.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/SM1.o: ../Generated_Code/SM1.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #26 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/SM1.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/SM1.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/SMasterLdd1.o: ../Generated_Code/SMasterLdd1.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #27 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/SMasterLdd1.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/SMasterLdd1.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/Vectors.o: ../Generated_Code/Vectors.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #28 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/Vectors.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/Vectors.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/WAIT1.o: ../Generated_Code/WAIT1.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #29 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/WAIT1.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/WAIT1.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
