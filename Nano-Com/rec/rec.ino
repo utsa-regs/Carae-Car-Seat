42d874abdda19d2ab08fba01ed5b296ce8b06a5e
inaruslynx <inaruslynx@gmail.com>
2019-04-05 23:55:20 -0500

    Massive Update to Code
    Code works very well between transmitters. Added code for key fob to notify parent. Still need to add code to sender to get data for status of child from KL25Z by reading a digital pin.

--- Nano-Com/rec/rec.ino
+++ Nano-Com/rec/rec.ino
@@ -1,40 +1,53 @@
-
-/*
-* Getting Started example sketch for nRF24L01+ radios
-* This is a very basic example of how to send data from one node to another
-* Updated: Dec 2014 by TMRh20
+/*  
+  * Group: REGS
+  * Programmer: Joshua Edwards 
+  * 4/5/2019
+  * This is the code for our key fob. It will transmitted to the Car seat
+  * and recieve the status of the child in the car seat. If the child 
+  * is in the seat as of the last update to the key fob and it is unable to 
+  * transmit due to loss of connection (the parent walks away) then 
+  * the buzzer will go off.
 */
 
-#include <SPI.h>
-#include "RF24.h"
+#include <SPI.h>                // Needed to talk to xmitter
+#include <RF24.h>
+#include <nRF24L01.h>
+#include <RF24_config.h>
 
-/****************** User Config ***************************/
+#define CE_PIN   7
+#define CSN_PIN  8
+#define BUZZ_PIN 5              //buzzer is on D5
+
 /* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
-RF24 radio(7, 8);
+RF24 radio(CE_PIN, CSN_PIN);
 /**********************************************************/
 
-byte addresses[][6] = {"1Node", "2Node"};
-
-// Used to control whether this node is sending or receiving
+byte addresses[2][6] = {"Node1", "Node2"};
 bool childInCar = false;
+bool sleep = false;
+unsigned long currentMillis;
+unsigned long prevMillis;
+unsigned long txIntervalMillis = 5000;  // check for 5 second gap
+unsigned long sleeptime = 20000;        //sleep for 20 seconds
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println(F("Reciever."));
-  //Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
+  pinMode(BUZZ_PIN, OUTPUT)
+  Serial.begin(9600);             // TODO: Remove this when I don't need to display to serial
+  Serial.println(F("Receiver.")); // TODO: Remove this when I don't need to display to serial
 
   radio.begin();
+  radio.setChannel(1);
+  radio.setAutoAck(1);            // Ensure autoACK is enabled
+  radio.setRetries(2, 15);        // Optionally, increase the delay between retries & # of retries
+  radio.setCRCLength(RF24_CRC_8); // Use 8-bit CRC for performance
 
-  // Set the PA Level low to prevent power supply related issues since this is a
-  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
-  radio.setPALevel(RF24_PA_LOW);
+  radio.setPALevel(RF24_PA_LOW);  // Using low power to save power and have range short on purpose
 
   // Open a writing and reading pipe on each radio, with opposite addresses
-  
-    radio.openWritingPipe(addresses[0]);
-    radio.openReadingPipe(1, addresses[1]);
-  
+
+  radio.openWritingPipe(addresses[1]);
+  radio.openReadingPipe(1, addresses[0]);
 
   // Start the radio listening for data
   radio.startListening();
@@ -42,21 +55,45 @@ void setup()
 
 void loop()
 {
-  bool recieved = false;
-
-  if (radio.available())
+  prevMillis = millis();                            // Get current time and put it into prevMillis
+  if (sleep)                                        // If the RF24 is asleep
   {
-    // Variable for the received timestamp
-    while (radio.available())
-    {                                        // While there is data ready
-      radio.read(&childInCar, sizeof(bool)); // Get the payload
-      recieved = true;
-      Serial.println(F("Received data."));
-      radio.stopListening();                // First, stop listening so we can talk
-      radio.write(&recieved, sizeof(bool)); // Send the final one back.
-      radio.startListening();               // Now, resume listening so we catch the next packets.
-      Serial.println(F("Sent response."));
+    currentMillis = millis();                       // Get time and put into currentMillis
+    while(currentMillis - prevMillis < sleeptime)   // While time is less than 20 secs
+    {
+      currentMillis = millis();                     // Get current time
+    }                                               // Come out of loop
+    sleep = false;                                  // Device coming out of sleep
+    radio.PowerUp();                                // Power up RF24
+    prevMillis = millis();                          // Put current time into prevMillis
+  }
+  while (!radio.available())                        // If there is nothing in the buffer we are gonna wait
+  {
+    currentMillis = millis();                       // Get current time while in loop
+    if (currentMillis - prevMillis >= txIntervalMillis) // If it has been longer than 5 seconds
+    {
+        if(childInCar)                              // If the child is in the seat
+        {
+          digitalWrite(BUZZ_PIN, HIGH);             // Set off the buzzer
+        }
+        else                                        // Otherwise
+        {
+          radio.powerDown();                        // It is safe to power down the RF24 for 20 seconds
+          sleep = true;                             // We are in sleep mode
+        }
     }
-  //end while loop
+  } //end while loop
+  if (radio.available())
+  {                                                 // If there is data ready
+    radio.read(&childInCar, sizeof(bool));          // Get the payload
+    Serial.println(F("Received data."));            // TODO: Remove this when I don't need to display to serial
+  }
+  if (childInCar)
+  {
+    Serial.println(F("Baby is in the seat!!!!!!!!!!!"));  // TODO: Remove this when I don't need to display to serial
+  }
+  else
+  {
+    Serial.println(F("Baby is not in the seat."));  // TODO: Remove this when I don't need to display to serial
   }
 }
--- Nano-Com/send/send.ino
+++ Nano-Com/send/send.ino
@@ -1,30 +1,39 @@
-
-/*
-* Getting Started example sketch for nRF24L01+ radios
-* This is a very basic example of how to send data from one node to another
-* Updated: Dec 2014 by TMRh20
+/*  
+  * Group: REGS
+  * Programmer: Joshua Edwards 
+  * 4/5/2019
+  * This is the code for our tranmitter. It will transmitted to the key fob
+  * and send the status of the child in the car seat. If the child 
+  * is in the seat as of the last update to the key fob and it is unable to 
+  * transmit due to loss of connection (the parent walks away) then 
+  * the buzzer on the key fob will go off.
 */
 
-#include <SPI.h>
-#include "RF24.h"
+#include <SPI.h>                // Needed to talk to xmitter
+#include <RF24.h>
+#include <nRF24L01.h>
+#include <RF24_config.h>
 
-/****************** User Config ***************************/
+#define CE_PIN   14
+#define CSN_PIN  10
+
 /* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
-RF24 radio(7, 8);
+RF24 radio(CE_PIN, CSN_PIN);
 /**********************************************************/
 
-byte addresses[][6] = {"1Node", "2Node"};
-
-// Used to control whether this node is sending or receiving
-bool childInCar = false;
+byte addresses[2][6] = { "Node1", "Node2" };
+bool childInCar = true;
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println(F("Transmitter."));
-  //Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
 
   radio.begin();
+  radio.setChannel(1);
+  radio.setAutoAck(1);                     // Ensure autoACK is enabled
+  radio.setRetries(2,15);                  // Optionally, increase the delay between retries & # of retries
+  radio.setCRCLength(RF24_CRC_8);          // Use 8-bit CRC for performance
 
   // Set the PA Level low to prevent power supply related issues since this is a
   // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
@@ -32,58 +41,25 @@ void setup()
 
   // Open a writing and reading pipe on each radio, with opposite addresses
 
-  radio.openWritingPipe(addresses[1]);
-  radio.openReadingPipe(1, addresses[0]);
-
-  // Start the radio listening for data
-  // radio.startListening();
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1, addresses[1]);
 }
 
 void loop()
 {
-
-  /****************** Ping Out Role ***************************/
-  //radio.stopListening();                                    // First, stop listening so we can talk.
-  Serial.println(F("Now sending"));
-  if (!radio.write(&childInCar, sizeof(childInCar)))
+  if (childInCar)
   {
-    Serial.println(F("failed"));
-  }
-
-  radio.startListening(); // Now, listening for response
-
-  unsigned long started_waiting_at = micros(); // Set up a timeout period, get the current microseconds
-  boolean timeout = false;                     // Set up a variable to indicate if a response was received or not
-
-  while (!radio.available())
-  { // While nothing is received
-    if (micros() - started_waiting_at > 100000)
-    { // If waited longer than 200ms, indicate timeout and exit while loop
-      timeout = true;
-      break;
-    }
-  }
-
-  if (timeout)
-  { // Describe the results
-    Serial.println(F("Failed, response timed out."));
+    Serial.println(F("Baby is in the seat."));
   }
   else
   {
-    bool received = false; // Grab the response, compare, and send to debugging spew
-    radio.read(&received, sizeof(received));
-
-    // Spew it
-    if (received)
-    {
-      Serial.println(F("Message was recieved."));
-    }
-    else
-    {
-      Serial.println(F("Message was not recieved."));
-    }
+    Serial.println(F("Baby is not in the seat."));
   }
-
+  Serial.println(F("Now sending"));
+  if (!radio.write(&childInCar, sizeof(bool)))
+  {
+    Serial.println(F("failed"));
+  }
   // Try again 1s later
   delay(1000);
 } // Loop
